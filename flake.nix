1d9767ed1584b3f8701ed0481c11d2d8551be3f9
LoC <loc@locrealloc.de>
2024-04-07 15:48:48 +0200

    Added the plugin jellyfin-plugin-kodisyncqueue

--- flake.nix
+++ flake.nix
@@ -7,78 +7,89 @@
       url = "file+https://github.com/9p4/jellyfin-plugin-sso/releases/download/v3.5.2.3/sso-authentication_3.5.2.3.zip";
       flake = false;
     };
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    lib = nixpkgs.lib;
-    plugins = import ./plugins.nix;
-
-    # at the moment, I just tested this flake on x86 Linux. If you got different hardware, please test this flake and create a PR!
-    defaultSystems = [
-      #"x86_64-darwin"
-      "x86_64-linux"
-      #"aarch64-linux"
-      #"aarch64-darwin"
-    ];
-
-    eachDefaultSystem = lib.genAttrs defaultSystems;
-  in {
-    packages = eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in (
-      builtins.mapAttrs
-      (name: value:
-        pkgs.stdenvNoCC.mkDerivation {
-            src = value;
-            pname = name;
-            nativeBuildInputs = with pkgs; [unzip];
-						version = plugins."${name}".version;
-            unpackPhase = ''
-              unzip ${value}
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp -R ./*.dll $out/.
-            '';
-          })
-      (lib.attrsets.getAttrs (lib.attrsets.mapAttrsToList (name: _: name) plugins) inputs)
-    ));
-    nixosModules.jellyfin-plugins = {
-      config,
-      pkgs,
-      ...
-    }: {
-      options.services.jellyfin = {
-        enabledPlugins = lib.mkOption {
-          type = lib.types.attrsOf lib.types.package;
-          default = {};
-        };
-      };
-      config = let
-        cfg = config.services.jellyfin;
-      in
-        lib.mkIf cfg.enable {
-          systemd.services.jellyfin.preStart =
-            ''
-              mkdir -p /var/lib/jellyfin/plugins
-            ''
-            + (
-              lib.strings.concatMapStrings
-              (
-                plugin: ''
-                  rm -rf /var/lib/jellyfin/plugins/${plugin.name}
-                  mkdir -p /var/lib/jellyfin/plugins/${plugin.name}
-                  ln -s ${plugin.path}/* /var/lib/jellyfin/plugins/${plugin.name}/.
-                  chmod -R 770 /var/lib/jellyfin/plugins/${plugin.name}
-                ''
-              )
-              (lib.attrsets.mapAttrsToList (name: path: {inherit name path;}) cfg.enabledPlugins)
-            );
-        };
+    "Jellyfin.Plugin.KodiSyncQueue" = {
+      url = "file+https://github.com/jellyfin/jellyfin-plugin-kodisyncqueue/releases/download/v9/kodi-sync-queue_9.0.0.0.zip";
+      flake = false;
     };
   };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      lib = nixpkgs.lib;
+      plugins = import ./plugins.nix;
+
+      # at the moment, I just tested this flake on x86 Linux. If you got different hardware, please test this flake and create a PR!
+      defaultSystems = [
+        #"x86_64-darwin"
+        "x86_64-linux"
+        #"aarch64-linux"
+        #"aarch64-darwin"
+      ];
+
+      eachDefaultSystem = lib.genAttrs defaultSystems;
+    in
+    {
+      formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+
+      packages = eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        (
+          builtins.mapAttrs
+            (name: value:
+              pkgs.stdenvNoCC.mkDerivation {
+                src = value;
+                pname = name;
+                nativeBuildInputs = with pkgs; [ unzip ];
+                version = plugins."${name}".version;
+                unpackPhase = ''
+                  unzip ${value}
+                '';
+                installPhase = ''
+                  mkdir -p $out
+                  cp -R ./*.dll $out/.
+                '';
+              })
+            (lib.attrsets.getAttrs (lib.attrsets.mapAttrsToList (name: _: name) plugins) inputs)
+        ));
+      nixosModules.jellyfin-plugins =
+        { config
+        , pkgs
+        , ...
+        }: {
+          options.services.jellyfin = {
+            enabledPlugins = lib.mkOption {
+              type = lib.types.attrsOf lib.types.package;
+              default = { };
+            };
+          };
+          config =
+            let
+              cfg = config.services.jellyfin;
+            in
+            lib.mkIf cfg.enable {
+              systemd.services.jellyfin.preStart =
+                ''
+                  mkdir -p /var/lib/jellyfin/plugins
+                ''
+                + (
+                  lib.strings.concatMapStrings
+                    (
+                      plugin: ''
+                        rm -rf /var/lib/jellyfin/plugins/${plugin.name}
+                        mkdir -p /var/lib/jellyfin/plugins/${plugin.name}
+                        ln -s ${plugin.path}/* /var/lib/jellyfin/plugins/${plugin.name}/.
+                        chmod -R 770 /var/lib/jellyfin/plugins/${plugin.name}
+                      ''
+                    )
+                    (lib.attrsets.mapAttrsToList (name: path: { inherit name path; }) cfg.enabledPlugins)
+                );
+            };
+        };
+    };
 }
