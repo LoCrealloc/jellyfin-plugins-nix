cbb0aa5bb34fa888af240928ef1b9437fb28e29d
LoC <loc@locrealloc.de>
2024-04-07 15:53:45 +0200

    added kodisyncqueue plugin to plugins file

--- plugins.nix
+++ plugins.nix
@@ -3,4 +3,8 @@
     version = "3.5.2.3";
     homepage = "https://github.com/9p4/jellyfin-plugin-sso";
   };
+  kodisyncqueue = {
+    version = "9.0.0.0";
+    homepage = "https://github.com/jellyfin/jellyfin-plugin-kodisyncqueue";
+  };
 }
