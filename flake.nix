447c03efed660c877e3791e797668d6019ee5cef
LoC <loc@locrealloc.de>
2024-10-25 18:08:30 +0200

    Changed formatter to nixfmt

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
       eachDefaultSystem = lib.genAttrs defaultSystems;
     in
     {
-      formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
       packages = eachDefaultSystem (
         system:
